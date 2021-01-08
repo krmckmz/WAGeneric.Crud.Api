3280829faf94b209b4b2d4e32be66c6891eab875
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2021-01-08 12:11:36 +0300

    SeedData migration class'ı eklendi.

--- Crud.Data/Migrations/20210106135537_SeedData.cs
+++ Crud.Data/Migrations/20210106135537_SeedData.cs
@@ -6,12 +6,32 @@ namespace Crud.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder
+                .Sql("INSERT INTO Projects (Name) Values ('Marsa')");
+            migrationBuilder
+                .Sql("INSERT INTO Projects (Name) Values ('Besler')");
+            migrationBuilder
+               .Sql("INSERT INTO Projects (Name) Values ('Sugav')");
+            migrationBuilder
+               .Sql("INSERT INTO Projects (Name) Values ('Mkk')");
 
+            migrationBuilder
+                .Sql("INSERT INTO Plugins (Title,Size, ProjectId) Values ('Unigate.Plugins.Parameter',355, (SELECT Id FROM Projects WHERE Name = 'Marsa'))");
+
+            migrationBuilder
+            .Sql("INSERT INTO Plugins (Title,Size, ProjectId) Values ('Unigate.Plugins.Language', 440,(SELECT Id FROM Projects WHERE Name = 'Besler'))");
+            migrationBuilder
+            .Sql("INSERT INTO Plugins (Title,Size, ProjectId) Values ('Unigate.Plugins.Contact', 380,(SELECT Id FROM Projects WHERE Name = 'Sugav'))");
+            migrationBuilder
+            .Sql("INSERT INTO Plugins (Title,Size, ProjectId) Values ('Unigate.Plugins.Blog',420, (SELECT Id FROM Projects WHERE Name = 'Mkk'))");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder
+                .Sql("Delete FROM Projects");
+            migrationBuilder
+                .Sql("Delete FROM Plugins");
         }
     }
 }
