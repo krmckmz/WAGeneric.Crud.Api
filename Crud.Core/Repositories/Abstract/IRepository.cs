7fbc79c97a3b1ed736f960b9456f29bf136919c2
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-03 19:48:10 +0300

    .

--- Crud.Core/Repositories/Abstract/IRepository.cs
+++ Crud.Core/Repositories/Abstract/IRepository.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace Crud.Core
 {
-    public interface IRepository<T> where T:class
+    public interface IRepository<T> where T : class
     {
         ValueTask<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
-        IEnumerable<T> Find(Expression<Func<T,bool>> predicate);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
         Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
         Task AddAsync(T item);
         Task AddRangeAsync(IEnumerable<T> entities);
